a2d0163076496a657c7080e3a34777924b67b492
Pbathin <athin04pb@gmail.com>
2025-08-05 16:17:07 +0530

    Updated

---
+++ src/components/UserProfile.jsx
@@ -0,0 +1,28 @@
+import { Avatar, Paper, Typography } from "@mui/material";
+
+const UserProfile = ({ data }) => {
+  return (
+    <Paper
+    square={false}
+      elevation={8}
+      sx={{
+        width: "250px",
+        padding: "20px",
+        textAlign: "center",
+        margin: "20px auto",
+        bgcolor:"#8bb6f7ff",
+      }}
+    >
+      <Avatar
+        src={data.avatar}
+        alt="User Avatar"
+        sx={{ width: "200px", height: "auto", margin: "10px auto" }}
+      />
+      <Typography variant="h5" sx={{color:"#fff", margin:"5px auto"}}>{data.name}</Typography>
+      <Typography variant="body1" sx={{color:"#fff" ,  margin:"5px auto"}}>{data.email}</Typography>
+      <Typography variant="body1" sx={{color:"#fff", margin:"5px auto    "}}>{data.role}</Typography>
+    </Paper>
+  );
+};
+
+export default UserProfile;
