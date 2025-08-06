9b1d785c4d833f07462d901eb99f1bc6300a1513
Pbathin <athin04pb@gmail.com>
2025-08-06 10:38:03 +0530

    Updated fonts styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
-import React from 'react'
 import UserProfile from './components/UserProfile'
 import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
 import theme from './theme';
-import './fonts/fonts.css';
 
 const App = () => {
   const user={
@@ -14,7 +11,6 @@ const App = () => {
   }
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline />
       <UserProfile data={user}/>
     </ThemeProvider>
   )
--- src/theme.js
+++ src/theme.js
@@ -2,8 +2,8 @@ import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
   typography: {
-    fontFamily: `'outfit', sans-serif`, 
+    fontFamily: `'Outfit', sans-serif`, 
   },
 });
 
-export default theme;
+export default theme
